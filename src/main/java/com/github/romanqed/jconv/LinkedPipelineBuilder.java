1e45efd18b8e632a6e99b0392b2351abea6c4250
RomanQed <gbakaldin@gmail.com>
2025-08-01 08:01:14 +0300

    Add javadocs for LinkedPipelineBuilder

--- src/main/java/com/github/romanqed/jconv/LinkedPipelineBuilder.java
+++ src/main/java/com/github/romanqed/jconv/LinkedPipelineBuilder.java
@@ -2,10 +2,25 @@ package com.github.romanqed.jconv;
 
 import java.util.LinkedList;
 
+/**
+ * A concrete {@link PipelineBuilder} implementation that uses a linked structure
+ * to compose and manage task pipelines.
+ *
+ * <p>This class extends {@link AbstractLinkedConfigurer} and provides full support for pipeline
+ * configuration and finalization. Once configured, the pipeline can be built into a {@link Task}
+ * using {@link #build()}.</p>
+ *
+ * <p>Tasks are stored internally in a {@link LinkedList}-backed deque.</p>
+ *
+ * @param <T> the type of input processed by the pipeline
+ */
 public final class LinkedPipelineBuilder<T>
         extends AbstractLinkedConfigurer<T, PipelineBuilder<T>>
         implements PipelineBuilder<T> {
 
+    /**
+     * Constructs a new, empty pipeline builder.
+     */
     public LinkedPipelineBuilder() {
         super(new LinkedList<>());
     }
